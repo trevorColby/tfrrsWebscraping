43dc03f084ed3844ed17003c5469f5d193b27801
trevorColby <trevor.john.colby@gmail.com>
2021-06-03 14:46:33 -0600

    dartmouthScrapying.py: updated to pull data from 3 rosters and use names for collision prevension

--- dartmouthScraping.py
+++ dartmouthScraping.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import csv
 import sys
@@ -48,63 +50,86 @@ class Athlete:
             athleteString = athleteString + result
         return athleteString
 
+def athleteNotSearched(name, athletes):
+    for athlete in athletes:
+        if athlete.getName() == name:
+            return False 
+    return True 
 # Setup selenium driver using chrome
 DRIVER_PATH = '/usr/local/bin/chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
+# pages = ['https://www.tfrrs.org/teams/NH_college_m_Dartmouth.html']
 # IMPORTANT: uncomment/comment below to swap between men and women's result scraping
 
+driver.get('https://www.tfrrs.org/teams/NH_college_f_Dartmouth.html')
 # driver.get('https://www.tfrrs.org/teams/NH_college_m_Dartmouth.html')
-driver.get('https://www.tfrrs.org/teams/NH_college_m_Dartmouth.html')
-
-# Once we have navigated to the team page, pull all of the tables on the page 
-all_tables = driver.find_elements(By.TAG_NAME, 'table')
-# The second table in our array is our team results
-teamResults = all_tables[1]
-# Pull out all of the links in the team results table, these will be our athlete names
-# as well as our links to each athlete's individual results
-links = teamResults.find_elements(By.TAG_NAME, 'a')
+selectors = ['[text="2019 Outdoor"]', '[text="2018 Outdoor"]', '[text="2017 NCAA Outdoor"]']
 
 athletes = []
-for link in links:
-    # make sure we are in the team homepage tab when we start each iteration
+for selector in selectors:
     driver.switch_to.window(driver.window_handles[0])
-    # create a new athlete object with the text from the link (i.e their name)
-    # Note: the constructor parses out the ',' to prevent malformating in the csv
-    athlete = Athlete(link.text)
-    # open the link to the athlete's results in a new tab
-    builder = ActionChains(driver);
-    openLinkInNewTab = builder.key_down(Keys.COMMAND).click(link).key_up(Keys.COMMAND);
-    openLinkInNewTab.perform();
-    # switch to the newly opened tab
-    driver.switch_to.window(driver.window_handles[1])
+    select = WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.TAG_NAME, 'select')))
+    actions = ActionChains(driver)
+    actions.move_to_element(select).perform()
+    # select = driver.find_elements(By.TAG_NAME, 'select')[0]
+    select.click()
+    outdoorOption = select.find_elements(By.CSS_SELECTOR, selector)[0]
+    outdoorOption.click()
 
-    # navigate to the list of the specific athlete's results 
-    athleteResults = driver.find_element(By.CSS_SELECTOR, '#meet-results')
-    # collect all of the meets, each meet is its own table
-    meets = athleteResults.find_elements(By.TAG_NAME, 'table')
-    for meet in meets:
-        # Get the title and date of the meet
-        header = meet.find_element(By.TAG_NAME, 'thead')
-        meetTitle = header.find_element(By.TAG_NAME, 'a').text
-        meetDate = header.find_element(By.TAG_NAME, 'span').text
-       
-        # the body of the table contains the rows of events
-        body = meet.find_element(By.TAG_NAME, 'tbody')
-        events = body.find_elements(By.TAG_NAME, 'tr')
-        for event in events:
-            data = event.find_elements(By.TAG_NAME, 'td')
-            # each row is split into 3 data parts, event name, time, and placement
-            eventName = data[0].text
-            eventTime = data[1].find_element(By.TAG_NAME, 'a').text
-            eventPlacement = data[2].text
+    # actionBuilder = ActionChains(driver);
+    #  = actionBuilder.click(select);
+    # openLinkInNewTab.perform();
 
-            # create a new result with the scraped info and add it to the athlete's array of results
-            resultObject = Result(eventName, eventTime, meetDate, meetTitle, eventPlacement) 
-            athlete.addResult(resultObject)
-    # Add our athlete to the array of athletes after we have iterated through all of their races
-    athletes.append(athlete)
-    driver.close()
+    # Once we have navigated to the team page, pull all of the tables on the page 
+    all_tables = driver.find_elements(By.TAG_NAME, 'table')
+    # The second table in our array is our team results
+    teamResults = all_tables[1]
+    # Pull out all of the links in the team results table, these will be our athlete names
+    # as well as our links to each athlete's individual results
+    links = teamResults.find_elements(By.TAG_NAME, 'a')
+    # links = links[:3]
+
+    for link in links:
+        # make sure we are in the team homepage tab when we start each iteration
+        driver.switch_to.window(driver.window_handles[0])
+        # create a new athlete object with the text from the link (i.e their name)
+        # Note: the constructor parses out the ',' to prevent malformating in the csv
+        if athleteNotSearched(link.text.replace(',',''), athletes):
+            athlete = Athlete(link.text)
+            # open the link to the athlete's results in a new tab
+            builder = ActionChains(driver);
+            openLinkInNewTab = builder.key_down(Keys.COMMAND).click(link).key_up(Keys.COMMAND);
+            openLinkInNewTab.perform();
+            # switch to the newly opened tab
+            driver.switch_to.window(driver.window_handles[1])
+
+            # navigate to the list of the specific athlete's results 
+            athleteResults = driver.find_element(By.CSS_SELECTOR, '#meet-results')
+            # collect all of the meets, each meet is its own table
+            meets = athleteResults.find_elements(By.TAG_NAME, 'table')
+            for meet in meets:
+                # Get the title and date of the meet
+                header = meet.find_element(By.TAG_NAME, 'thead')
+                meetTitle = header.find_element(By.TAG_NAME, 'a').text
+                meetDate = header.find_element(By.TAG_NAME, 'span').text
+                if ('2017' in meetDate) or ('2018' in meetDate) or  ('2019' in meetDate):
+                    # the body of the table contains the rows of events
+                    body = meet.find_element(By.TAG_NAME, 'tbody')
+                    events = body.find_elements(By.TAG_NAME, 'tr')
+                    for event in events:
+                        data = event.find_elements(By.TAG_NAME, 'td')
+                        # each row is split into 3 data parts, event name, time, and placement
+                        eventName = data[0].text
+                        eventTime = data[1].find_element(By.TAG_NAME, 'a').text.replace(',','')
+                        eventPlacement = data[2].text
+
+                        # create a new result with the scraped info and add it to the athlete's array of results
+                        resultObject = Result(eventName, eventTime, meetDate, meetTitle, eventPlacement) 
+                        athlete.addResult(resultObject)
+            # Add our athlete to the array of athletes after we have iterated through all of their races
+            athletes.append(athlete)
+            driver.close()
 
 # Check sys arguments to decide where to write our data to 
 outputLocation = 'results.csv'
